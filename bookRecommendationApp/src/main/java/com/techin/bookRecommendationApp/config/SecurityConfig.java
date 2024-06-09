489665d5212ae13b36d9dcc348d30dd7e69fdb7c
Anton Isakov <anton.isakov@stud.techin.lt>
2024-06-09 22:38:09 +0300

    back end project created

---
+++ bookRecommendationApp/src/main/java/com/techin/bookRecommendationApp/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.techin.bookRecommendationApp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .headers(headers -> headers
+                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+                .build();
+    }
+}
---
+++ bookRecommendationApp/src/test/java/com/techin/bookRecommendationApp/BookRecommendationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techin.bookRecommendationApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookRecommendationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
