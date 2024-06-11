ac2bf280034787906cdf136c1b58fd7900d8387f
Anton Isakov <anton.isakov@stud.techin.lt>
2024-06-11 19:57:14 +0300

    login with token works

--- bookRecommendationApp/src/main/java/com/techin/bookRecommendationApp/config/SecurityConfig.java
+++ bookRecommendationApp/src/main/java/com/techin/bookRecommendationApp/config/SecurityConfig.java
@@ -1,7 +1,10 @@
 package com.techin.bookRecommendationApp.config;
 
+import jakarta.servlet.Filter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -13,18 +16,39 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
+
+        http
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")).permitAll()
-                        .anyRequest().authenticated()
+                .authorizeHttpRequests(
+                        req -> req.requestMatchers("/login/**", "/register/**", "/h2-console/**", "/api/v1/auth/**")
+                                .permitAll()
+                                .requestMatchers("/admin_only/**")
+                                .hasAnyAuthority("ADMIN")
+                                .anyRequest()
+                                .authenticated()
                 )
-                .headers(headers -> headers
-                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
-                .build();
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .headers(headers -> headers   //to connect H2 database
+                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
+        return http.build();
+
+//        return http
+//                .csrf(AbstractHttpConfigurer::disable)
+//                .authorizeHttpRequests(authorize -> authorize
+//                        .requestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")).permitAll()
+//                        .anyRequest().authenticated()
+//                )
+//                .headers(headers -> headers
+//                        .frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+//                .build();
     }
 }
